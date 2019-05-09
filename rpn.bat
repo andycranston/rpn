aa4d18616758dbb8eacc7b6e23d4400826a7b900
andycranston <acranston@gmail.com>
2019-05-09 12:27:48 +0100

    Added REM statements and fullpath to rpn.py

--- rpn.bat
+++ rpn.bat
@@ -1,1 +1,12 @@
-@python rpn.py
+@ECHO OFF
+
+REM
+REM @(!--#) @(#) rpn.bat, version 001, 09-may-2019
+REM
+REM Short BAT (batch) file to run the Python program rpn.py
+REM
+REM Copy this BAT file to a directory in your PATH
+REM environment variable such as C:\Windows
+REM
+
+python c:\andyc\projects\rpn\rpn.py
